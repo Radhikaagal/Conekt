8c347e76222fed9e4755396ff940d36e2d9caacb
Radhikaagal <radhika.agal@gmail.com>
2019-07-31 12:33:45 +0530

    second commit

---
+++ src/components/Homepage.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import "./../styles/Homepage.css";
+import logo from "./../Images/homepageimg.jpg";
+
+class Homepage extends Component{
+    render()
+    {
+        return(
+        <img src={logo} className="bgimg" />
+            
+        
+        
+    
+            
+        );
+    }
+}
+
+export default Homepage;
